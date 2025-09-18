0b05a172861f4ac4978b8a83e297b018dc23fa18
Cauã Diego <tixyelpvd@gmail.com>
2025-09-18 10:04:33 -0300

    feat 🎉: update ClientEvents type to use specific Youtube data type

--- packages/zynex/src/types/client.ts
+++ packages/zynex/src/types/client.ts
@@ -1,10 +1,11 @@
 import { Twitch } from './streamelements/events/twitch/index.js'
+import { Youtube } from './streamelements/events/youtube/index.js'
 
 export type Provider = 'twitch' | 'youtube' | 'kick' | 'facebook' | 'streamelements'
 
 export type ClientEvents =
   | { provider: 'twitch'; data: Twitch }
-  | { provider: 'youtube'; data: unknown }
+  | { provider: 'youtube'; data: Youtube }
   | { provider: 'kick'; data: unknown }
   | { provider: 'facebook'; data: unknown }
   | { provider: 'streamelements'; data: unknown }
