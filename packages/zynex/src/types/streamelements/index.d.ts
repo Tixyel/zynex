10ae42873c139722af527473e03e80769755fb39
Cauã Diego <tixyelpvd@gmail.com>
2025-09-18 08:52:54 -0300

    feat 🎉: add StreamElements API types for session, follower, subscriber, host, raid, charity donations, cheers, superchat, channel points, tips, merch, and purchases

---
+++ packages/zynex/src/types/streamelements/index.d.ts
@@ -0,0 +1,16 @@
+declare const SE_API: {
+  responses: Record<string, any>
+  sendMessage: (message: string, data: object) => void
+  counters: {
+    get: (key: string) => number | null
+  }
+  store: {
+    get: <T>(key: string) => Promise<T | null>
+    set: <T>(key: string, value: T) => void
+  }
+  resumeQueue: () => void
+  sanitize: (message: string) => string
+  cheerFilter: (message: string) => string
+  setField: (key: string, value: string | number | boolean | undefined, reload: boolean) => void
+  getOverlayStatus: () => { isEditorMode: boolean; muted: boolean }
+}
