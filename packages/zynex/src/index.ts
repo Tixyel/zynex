404bd65d9decbb1c872a1eb63ead1fdd827490a5
Cauã Diego <tixyelpvd@gmail.com>
2025-09-18 08:53:20 -0300

    feat 🎉: update global types to include onEventReceived and onSessionUpdate events

--- packages/zynex/src/index.ts
+++ packages/zynex/src/index.ts
@@ -1,8 +1,22 @@
-import './global.d.ts'
 import { Client } from './client/index.js'
+import { onEventReceivedEvent } from './types/streamelements/events/onEventReceived.js'
+import { onSessionUpdateEvent } from './types/streamelements/events/onSessionUpdate.js'
+import { onWidgetLoadEvent } from './types/streamelements/events/onWidgetLoad.js'
 
 export const Zynex = {
   Client: Client,
+}
+
+declare global {
+  interface Window {
+    Zynex: typeof Zynex
+    client?: Client
+  }
+  interface WindowEventMap {
+    'onWidgetLoad': onWidgetLoadEvent
+    'onSessionUpdate': onSessionUpdateEvent
+    'onEventReceived': onEventReceivedEvent
+  }
 }
 
 if (typeof window !== 'undefined') {
