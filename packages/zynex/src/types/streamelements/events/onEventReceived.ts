47b2a0abc6d927b6ed156a6d2323a85a074684ff
Cauã Diego <tixyelpvd@gmail.com>
2025-09-18 10:04:37 -0300

    feat 🎉: extend onEventReceived type to include Youtube alongside Twitch and StreamElements

--- packages/zynex/src/types/streamelements/events/onEventReceived.ts
+++ packages/zynex/src/types/streamelements/events/onEventReceived.ts
@@ -1,5 +1,7 @@
+import { StreamElements } from './integrated/index.js'
 import { Twitch } from './twitch/index.js'
+import { Youtube } from './youtube/index.js'
 
 export type onEventReceivedEvent = CustomEvent<onEventReceived>
 
-export type onEventReceived = Twitch
+export type onEventReceived = Twitch | Youtube | StreamElements
