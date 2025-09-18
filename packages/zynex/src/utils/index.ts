34a9333aeb23fc55497845346302e5f9f3630314
Cauã Diego <tixyelpvd@gmail.com>
2025-09-18 08:53:48 -0300

    feat 🎉: add utility types for path value extraction and number-to-string mapping

---
+++ packages/zynex/src/utils/index.ts
@@ -0,0 +1,13 @@
+export type PathValue<T, P extends string> = P extends `${infer K}.${infer Rest}`
+  ? K extends keyof T
+    ? PathValue<T[K], Rest>
+    : never
+  : P extends keyof T
+  ? T[P]
+  : never
+
+export type NumberAsString<T extends number | bigint | string> = ReturnType<T['toString']>
+
+export type MapNumberValuesToString<T> = {
+  [K in keyof T]: T[K] extends number ? NumberAsString<T[K]> : T[K]
+}
