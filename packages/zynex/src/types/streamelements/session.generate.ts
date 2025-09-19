4d04c5059b326ce9f6cea0d46c611c00fd4aaff3
Cauã Diego <tixyelpvd@gmail.com>
2025-09-19 05:21:39 -0300

    feat 🎉: add StreamElementsField and Session configuration types for enhanced field management

---
+++ packages/zynex/src/types/streamelements/session.generate.ts
@@ -0,0 +1,41 @@
+export type Session$AnyConfig =
+  | Session$IntConfig
+  | Session$StringConfig
+  | Session$DateConfig
+  | Session$ArrayConfig
+  | Session$RecentConfig
+  | undefined
+
+export type Session$AvailableCategory = Record<
+  string,
+  Session$AnyConfig | Record<string, Session$AnyConfig | undefined> | undefined
+>
+
+export type Session$AvailableData = Record<string, Session$AvailableCategory | undefined>
+
+export type Session$IntConfig = {
+  type: 'int'
+  min: number
+  max: number
+}
+
+export type Session$StringConfig = {
+  type: 'string'
+  options: string[]
+}
+
+export type Session$ArrayConfig = {
+  type: 'array'
+  options: any[]
+}
+
+export type Session$DateConfig = {
+  type: 'date'
+  range: number
+}
+
+export type Session$RecentConfig = {
+  type: 'recent'
+  amount: number
+  value: Record<string, Session$AnyConfig>
+}
